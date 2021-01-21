cdf6ae9c19c8cd7ed9c3fef29225de40b80bf208
Sergey <kaplin@internet.ru>
2021-01-21 17:31:10 +0300

    cabinet add
    datatables avalible

--- views/cabinet/cabinet.tpl.php
+++ views/cabinet/cabinet.tpl.php
@@ -19,6 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="../../css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../css/datatables.min.css"/>
 
 </head>
 
@@ -423,6 +424,105 @@
                         </div>
                     </div>
 
+
+                    <div class="row">
+
+<!-- Area Chart -->
+<div class="col-xl-12 col-lg-12">
+    <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div
+            class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Список контрактов</h6>
+            <div class="dropdown no-arrow">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                    aria-labelledby="dropdownMenuLink">
+                    <div class="dropdown-header">Dropdown Header:</div>
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+            <div class="chart-area">
+            <table id="table_id" class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>№ контракта</th>
+            <th>Дата заключения</th>
+            <th>Предмет контракта</th>
+            <th>Периодичность</th>
+            <th>Крайняя дата</th>
+           
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="bg-success text-light">
+            <td>1</td>
+            <td>25554</td>
+            <td>25.12.2020</td>
+            <td>Поставка окон</td>
+            <td>1</td>
+            <td>25.01.2020</td>
+        </tr>
+        <tr>
+        <td>2</td>
+            <td>25456</td>
+            <td>26.12.2020</td>
+            <td>Поставка дверей</td>
+            <td>1</td>
+            <td>26.01.2020</td>
+            
+        </tr>
+        <tr class="bg-primary text-light">
+            <td>3</td>
+            <td>25554</td>
+            <td>25.12.2020</td>
+            <td>Поставка ключей</td>
+            <td>1</td>
+            <td>25.01.2020</td>
+        </tr>
+        <tr class="bg-danger text-light">
+            <td>4   </td>
+            <td>25554</td>
+            <td>25.12.2020</td>
+            <td>Поставка бумаги</td>
+            <td>1</td>
+            <td>25.01.2020</td>
+        </tr>
+        <tr class="bg-danger text-light">
+            <td>5  </td>
+            <td>25554</td>
+            <td>25.12.2020</td>
+            <td>Поставка бумаги</td>
+            <td>1</td>
+            <td>25.01.2020</td>
+        </tr>
+        <tr class="">
+            <td>6</td>
+            <td>25554</td>
+            <td>25.12.2020</td>
+            <td>Поставка бумаги</td>
+            <td>1</td>
+            <td>25.01.2020</td>
+        </tr>
+    </tbody>
+</table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
                     <!-- Content Row -->
                
 
@@ -489,6 +589,51 @@
     <!-- Page level custom scripts -->
     <script src="../../js/demo/chart-area-demo.js"></script>
     <script src="../../js/demo/chart-pie-demo.js"></script>
+    <script type="text/javascript" src="../../js/datatables.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready( function () {
+    $('#table_id').DataTable(
+     
+        {
+            select: true,
+            
+            "language":
+
+{
+  "processing": "Подождите...",
+  "search": "Поиск:",
+  "lengthMenu": "Показать _MENU_ записей",
+  "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+  "infoEmpty": "Записи с 0 до 0 из 0 записей",
+  "infoFiltered": "(отфильтровано из _MAX_ записей)",
+  "loadingRecords": "Загрузка записей...",
+  "zeroRecords": "Записи отсутствуют.",
+  "emptyTable": "В таблице отсутствуют данные",
+  "paginate": {
+    "first": "Первая",
+    "previous": "Предыдущая",
+    "next": "Следующая",
+    "last": "Последняя"
+  },
+  "aria": {
+    "sortAscending": ": активировать для сортировки столбца по возрастанию",
+    "sortDescending": ": активировать для сортировки столбца по убыванию"
+  },
+  "select": {
+    "rows": {
+      "_": "Выбрано записей: %d",
+      "0": "Кликните по записи для выбора",
+      "1": "Выбрана одна запись"
+    }
+  }
+}
+        }
+    );
+
+    
+} );
+
+    </script>
 
 </body>
 
